741fbf98f2056f59b41ea8dffd4271ac213d8935
Armando Guarino <arm.guarino@gmail.com>
2021-03-13 16:53:29 +0100

    add pages

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,33 @@
-import {
-  Image,
-  Button,
-  ButtonGroup,
-  Flex,
-  Box,
-  Container,
-} from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import styles from "../styles/Home.module.css";
+import { Image, Button, Flex, Box, Container } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Container minH="90vh" maxW="container.lg" mt={4} border="2px solid red">
+    <Container minH="90vh" maxW="container.lg" mt={4}>
       <Flex>
-        <Box maxW="50%">
-          <h1>Full-stack Software Developer.</h1>
-          <p>
-            Interested in developing web applications that bring social change.{" "}
-          </p>
+        <Flex
+          maxW="50%"
+          flexDirection="column"
+          alignItems="flex-start"
+          justifyContent="center"
+          border="2px solid red"
+        >
+          <Box my={4}>
+            <h1>Full-stack Software Developer.</h1>
+            <p>
+              Interested in developing web applications that bring social
+              change.{" "}
+            </p>
+          </Box>
           <Button colorScheme="blue">Button</Button>
-        </Box>
-        <Image
-          src="/images/pic.jpg"
-          fallbackSrc="placeholdit.com/200x200"
-          alt="A Placeholder Image"
-          width="250px"
-        />
+        </Flex>
+        <Flex border="2px solid red" justifyContent="center" minW="50%">
+          <Image
+            src="/images/pic.jpg"
+            fallbackSrc="placeholdit.com/200x200"
+            alt="A Placeholder Image"
+            width="250px"
+          />
+        </Flex>
       </Flex>
     </Container>
   );
