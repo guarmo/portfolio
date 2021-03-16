11e2fd5a9aa94538dbacc1ae4bc90d9b101b6597
Armando Guarino <arm.guarino@gmail.com>
2021-03-16 11:27:24 +0100

    add drawer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,80 @@
+import React from "react";
 import {
   Link,
   Flex,
   Icon,
   HStack,
+  VStack,
   useColorMode,
   Button,
+  UnorderedList,
+  Heading,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { FaMoon, FaRegMoon } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [placement, setPlacement] = React.useState("left");
 
   return (
     <nav>
       <Flex alignItems="center" justifyContent="space-between">
+        <Icon
+          cursor="pointer"
+          display={{ base: "block", md: "none" }}
+          w={8}
+          h={8}
+          as={GiHamburgerMenu}
+          onClick={onOpen}
+        >
+          Open
+        </Icon>
+        <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay>
+            <DrawerContent p={4}>
+              <Icon
+                cursor="pointer"
+                onClick={onClose}
+                w={8}
+                h={8}
+                as={AiOutlineClose}
+              />
+              <DrawerBody>
+                <Flex
+                  flexDirection="col"
+                  alignItems="center"
+                  justifyContent="center"
+                  h="100%"
+                >
+                  <VStack spacing="40px" fontWeight="bold">
+                    <Link href="#top">home.</Link>
+                    <Link href="#about">about me.</Link>
+                    <Link href="#projects">projects.</Link>
+                    <Link href="#form">contact.</Link>
+                  </VStack>
+                </Flex>
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
         <h1 className="logo">ag</h1>
-        <ul>
+        <UnorderedList display={{ base: "none", md: "block" }}>
           <HStack spacing="10px" fontWeight="bold">
             <Link href="#top">home.</Link>
             <Link href="#about">about me.</Link>
             <Link href="#projects">projects.</Link>
             <Link href="#form">contact.</Link>
           </HStack>
-        </ul>
+        </UnorderedList>
         <Button onClick={toggleColorMode}>
           {colorMode === "light" ? (
             <Icon as={FaRegMoon} />
