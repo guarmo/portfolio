9ceb109b5a2bc0c455612f3e80b15976cff1a172
Armando Guarino <arm.guarino@gmail.com>
2021-04-02 12:05:45 +0200

    modify meta tags

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Armando Guarino | Portfolio</title>
-        {/* <meta charset="UTF-8" />
+        <meta charset="UTF-8" />
         <meta
           name="description"
           content="Armando Guarino's personal portfolio."
@@ -21,12 +21,15 @@ export default function Home() {
           content="Armando Guarino, guarino, guarmo, portfolio"
         />
         <meta name="author" content="Armando Guarino" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content="https://www.armandoguarino.dev/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Armando Guarino | Portfolio" />
         <meta name="twitter:card" content="summary" />
-        <meta property="og:image" content="https://pasteboard.co/JTOsPz6.png" />
+        <meta
+          property="og:image"
+          content="https://i.postimg.cc/28bsB83K/Screenshot.png"
+        />
         <meta
           property="og:description"
           content="Armando Guarino's personal portfolio."
